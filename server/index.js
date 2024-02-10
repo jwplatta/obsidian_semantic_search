8891adc835fae45d78e2cfc9324565725e615f9d
Joseph Platta <jwplatta@gmail.com>
2024-02-10 16:37:06 -0600

    debug server endpoints - still need to clean this up

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import { pipeline } from '@xenova/transformers';
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import * as tf from '@tensorflow/tfjs';
+import { checkFileExists } from './src/util.js';
 
 const app = express();
 app.use(express.json());
@@ -14,6 +16,7 @@ app.get('/check_status', (req, res) => {
   res.sendStatus(200);
 });
 
+
 app.post('/configure_db', (req, res) => {
   // TODO: check if running docker container
   const joinedPath = path.join(
@@ -22,10 +25,13 @@ app.post('/configure_db', (req, res) => {
     'semantic_search.db'
   );
 
+  if (!checkFileExists(joinedPath)) {
+    res.sendStatus(200);
+    return;
+  }
+
   const db = new sqlite3(joinedPath);
   sqlite_vss.load(db);
-  // const version = db.prepare("SELECT vss_version();").pluck().get();
-  // console.log("vss_version: ", version);
 
   const createEmbeddingsTable = `
     CREATE TABLE IF NOT EXISTS note_chunks (
@@ -55,6 +61,125 @@ app.post('/configure_db', (req, res) => {
   res.sendStatus(200);
 });
 
+app.post('/create_embedding', async (req, res) => {
+  const fileName = req.body.fileName;
+  const joinedPath = path.join(
+    req.body.vaultPath,
+    req.body.dataStorePath,
+    'semantic_search.db'
+  );
+  const db = new sqlite3(joinedPath);
+  sqlite_vss.load(db);
+
+  const filePath = req.body.filePath;
+  const fileContent = req.body.fileContent;
+  const chunkSize = req.body.chunkSize;
+  const embedder = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: chunkSize,
+    chunkOverlap: 50
+  });
+  const chunks = await splitter.createDocuments([fileContent]);
+  db.exec('BEGIN TRANSACTION');
+  const insertPromises = chunks.map(async(chunk, index) => {
+    try {
+      const embeddings = await embedder([chunk.pageContent]);
+      const meanTensor = tf.tensor(embeddings[0]['data'])
+          .reshape(embeddings[0]['dims'])
+          .mean(0);
+      const embeddingJSON = JSON.stringify(meanTensor.arraySync());
+
+      db.prepare('INSERT INTO note_chunks (file_name, file_path, text_chunk, embedding) VALUES (?, ?, ?, ?)').run(fileName, filePath, chunk.pageContent, embeddingJSON);
+    } catch (error) {
+      db.exec('ROLLBACK');
+      console.error(error);
+      res.sendStatus(500);
+    }
+  });
+
+  await Promise.all(insertPromises);
+  db.exec('COMMIT');
+
+  res.sendStatus(200);
+});
+
+app.post('/delete_embedding', async (req, res) => {
+  const fileName = req.body.fileName;
+  const joinedPath = path.join(
+    req.body.vaultPath,
+    req.body.dataStorePath,
+    'semantic_search.db'
+  );
+  const db = new sqlite3(joinedPath);
+  sqlite_vss.load(db);
+
+  db.exec('BEGIN TRANSACTION');
+  try {
+    db.prepare(
+      'DELETE FROM vss_note_chunks WHERE rowid IN (SELECT rowid FROM note_chunks WHERE file_name = ?)'
+    ).run(fileName);
+  } catch (error) {
+    db.exec('ROLLBACK');
+    console.error("removeEmbeddingsFromVSSStmt: ", error);
+    res.sendStatus(500);
+  }
+
+  try {
+    db.prepare('DELETE FROM note_chunks WHERE file_name = ?').run(fileName);
+  } catch (error) {
+    db.exec('ROLLBACK');
+    console.error("removeEmbeddingsStmt: ", error);
+    res.sendStatus(500);
+  }
+
+  db.exec('COMMIT');
+
+  res.sendStatus(200);
+});
+
+app.post('/update_index', async (req, res) => {
+  const fileName = req.body.fileName;
+  const joinedPath = path.join(
+    req.body.vaultPath,
+    req.body.dataStorePath,
+    'semantic_search.db'
+  );
+  const db = new sqlite3(joinedPath);
+  sqlite_vss.load(db);
+
+  // STEP: update the virtual table
+  const insertEmbeddingsIntoVSS = `
+    INSERT INTO vss_note_chunks(rowid, embedding)
+    SELECT rowid, embedding
+    FROM note_chunks
+    WHERE file_name = ?
+  `;
+
+  try {
+    db.exec('BEGIN TRANSACTION');
+    const insertEmbeddingsIntoVSSStmt = db.prepare(insertEmbeddingsIntoVSS);
+    insertEmbeddingsIntoVSSStmt.run(fileName);
+    db.exec('COMMIT');
+  } catch (error) {
+    db.exec('ROLLBACK');
+    console.error(insertEmbeddingsIntoVSS, error);
+    res.sendStatus(500);
+  }
+
+  try {
+    const vssCountStmt = db.prepare('SELECT count(1) FROM vss_note_chunks');
+    const vssCount = vssCountStmt.pluck().get();
+    console.log("vssCount ", vssCount);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+
+  res.sendStatus(200);
+
+});
+
 app.post('/embed', async (req, res) => { //TODO: remove async
   const joinedPath = path.join(
     req.body.vaultPath,
@@ -64,49 +189,106 @@ app.post('/embed', async (req, res) => { //TODO: remove async
   const db = new sqlite3(joinedPath);
   sqlite_vss.load(db);
 
-  // STEP: delete embeddings if they exist
   const fileName = req.body.fileName;
-  let embeddingsCount;
+  db.exec('BEGIN TRANSACTION');
+  // STEP: delete embeddings if they exist. Delete from virtual store first
+  const removeEmbeddingsFromVSSStmt = db.prepare(
+    'DELETE FROM vss_note_chunks WHERE rowid IN (SELECT rowid FROM note_chunks WHERE file_name = ?)'
+  );
   try {
-    const embeddingsCountStmt = db.prepare('SELECT count(1) FROM note_chunks WHERE file_name = ?');
-    console.log(embeddingsCountStmt);
-    embeddingsCount = embeddingsCountStmt.pluck().get(fileName);
-    console.log('embeddingsCount: ', embeddingsCount);
+    removeEmbeddingsFromVSSStmt.run(fileName);
   } catch (error) {
-    console.error(error);
+    db.exec('ROLLBACK');
+    console.error("removeEmbeddingsFromVSSStmt: ", error);
     res.sendStatus(500);
   }
 
-  if (embeddingsCount > 0) {
-    const removeEmbeddingsStmt = db.prepare('DELETE FROM note_chunks WHERE file_name = ?');
-    try {
-      removeEmbeddingsStmt.run(fileName);
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
-    }
+  const removeEmbeddingsStmt = db.prepare('DELETE FROM note_chunks WHERE file_name = ?');
+  try {
+    removeEmbeddingsStmt.run(fileName);
+    db.exec('COMMIT');
+  } catch (error) {
+    db.exec('ROLLBACK');
+    console.error("removeEmbeddingsStmt: ", error);
+    res.sendStatus(500);
   }
+
+  // try {
+  //   const embeddingsCountStmt = db.prepare('SELECT count(1) FROM note_chunks WHERE file_name = ?');
+  //   const embeddingsCount = embeddingsCountStmt.pluck().get(fileName);
+  //   console.log("embeddingsCount ", embeddingsCount, " for ", fileName);
+  // } catch (error) {
+  //   console.error(error);
+  //   res.sendStatus(500);
+  // }
 
   // STEP: generate the embeddings and insert them into the database
   const filePath = req.body.filePath;
   const fileContent = req.body.fileContent;
   const chunkSize = req.body.chunkSize;
   const embedder = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
-  const stmt = db.prepare('INSERT INTO note_chunks (file_name, file_path, text_chunk, embedding) VALUES (?, ?, ?, ?)');
 
-  for (let i = 0; i < fileContent.length; i += chunkSize) {
+  // for (let i = 0; i < fileContent.length; i += chunkSize) {
+  //   try {
+  //     const chunk = fileContent.substring(i, i + chunkSize);
+  //     const embeddings = await embedder([chunk]);
+  //     const meanTensor = tf.tensor(embeddings[0]['data'])
+  //       .reshape(embeddings[0]['dims'])
+  //       .mean(0);
+  //     const embeddingJSON = JSON.stringify(meanTensor.arraySync());
+  //     db.prepare('INSERT INTO note_chunks (file_name, file_path, text_chunk, embedding) VALUES (?, ?, ?, ?)').run(fileName, filePath, chunk, embeddingJSON);
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.sendStatus(500);
+  //   }
+  // }
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: chunkSize,
+    chunkOverlap: 50
+  });
+  const chunks = await splitter.createDocuments([fileContent]);
+  db.exec('BEGIN TRANSACTION');
+  const insertPromises = chunks.map(async(chunk, index) => {
     try {
-      const chunk = fileContent.substring(i, i + chunkSize);
-      const embeddings = await embedder([chunk]);
+      const embeddings = await embedder([chunk.pageContent]);
       const meanTensor = tf.tensor(embeddings[0]['data'])
-        .reshape(embeddings[0]['dims'])
-        .mean(0);
+          .reshape(embeddings[0]['dims'])
+          .mean(0);
       const embeddingJSON = JSON.stringify(meanTensor.arraySync());
-      stmt.run(fileName, filePath, chunk, embeddingJSON);
+
+      db.prepare('INSERT INTO note_chunks (file_name, file_path, text_chunk, embedding) VALUES (?, ?, ?, ?)').run(fileName, filePath, chunk.pageContent, embeddingJSON);
     } catch (error) {
+      db.exec('ROLLBACK');
       console.error(error);
       res.sendStatus(500);
     }
+  });
+  // chunks.forEach(async(chunk, index) => {
+  //   try {
+  //     const embeddings = await embedder([chunk.pageContent]);
+  //     const meanTensor = tf.tensor(embeddings[0]['data'])
+  //       .reshape(embeddings[0]['dims'])
+  //       .mean(0);
+  //     const embeddingJSON = JSON.stringify(meanTensor.arraySync());
+
+  //     db.prepare('INSERT INTO note_chunks (file_name, file_path, text_chunk, embedding) VALUES (?, ?, ?, ?)').run(fileName, filePath, chunk.pageContent, embeddingJSON);
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.sendStatus(500);
+  //   }
+  // });
+
+  await Promise.all(insertPromises);
+  db.exec('COMMIT');
+
+  try {
+    const noteChunksCountStmt = db.prepare('SELECT count(1) FROM note_chunks WHERE file_name = ?');
+    const noteChunksCount = noteChunksCountStmt.pluck().get(fileName);
+    console.log("noteChunksCountB: ", noteChunksCount);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
   }
 
   // STEP: update the virtual table
@@ -114,30 +296,39 @@ app.post('/embed', async (req, res) => { //TODO: remove async
     INSERT INTO vss_note_chunks(rowid, embedding)
     SELECT rowid, embedding
     FROM note_chunks
-    WHERE rowid NOT IN (SELECT rowid FROM vss_note_chunks)
+    WHERE file_name = ?
   `;
 
   try {
-    db.prepare(insertEmbeddingsIntoVSS).run();
+    db.exec('BEGIN TRANSACTION');
+    const insertEmbeddingsIntoVSSStmt = db.prepare(insertEmbeddingsIntoVSS);
+    insertEmbeddingsIntoVSSStmt.run(fileName);
+    db.exec('COMMIT');
   } catch (error) {
+    db.exec('ROLLBACK');
     console.error(insertEmbeddingsIntoVSS, error);
     res.sendStatus(500);
   }
 
-  res.sendStatus(200);
-});
+  try {
+    const vssCountStmt = db.prepare('SELECT count(1) FROM vss_note_chunks');
+    const vssCount = vssCountStmt.pluck().get();
+    console.log("vssCount ", vssCount);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 
-app.post('/embed_multiple', (req, res) => {
-  console.log('files recieved: ', req.body);
+  console.log("Embedded: ", fileName);
   res.sendStatus(200);
 });
 
 // NOTE: this needs to async
 app.post('/query', async (req, res) => {
-  console.log('QUERY recieved: ', req.body.query);
+  console.log('QUERY recieved: ', req.body);
 
   if (req.body.query === '') {
-    res.sendStatus(200);
+    res.status(200).json([]);
     return;
   }
 
@@ -146,6 +337,8 @@ app.post('/query', async (req, res) => {
     req.body.dataStorePath,
     'semantic_search.db'
   );
+
+  console.log('database path: ', joinedPath);
 
   const db = new sqlite3(joinedPath);
   sqlite_vss.load(db);
@@ -177,14 +370,24 @@ app.post('/query', async (req, res) => {
       note_chunks.text_chunk,
       matches.distance
     FROM matches
-    LEFT JOIN note_chunks ON note_chunks.rowid = matches.rowid;
+    INNER JOIN note_chunks ON note_chunks.rowid = matches.rowid;
   `;
+
   try {
-    console.log(searchEmbeddings);
+    const vssCountStmt = db.prepare('SELECT count(1) FROM vss_note_chunks');
+    const vssCount = vssCountStmt.pluck().get();
+    console.log("vssCount ", vssCount);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+
+  try {
     const searchResults = db.prepare(searchEmbeddings).all(
       "[" + queryEmbedding.arraySync().toString() + "]",
       searchResultsCount
     );
+    console.log(searchResults);
     res.status(200).json(searchResults);
   } catch (error) {
     console.error(error);
