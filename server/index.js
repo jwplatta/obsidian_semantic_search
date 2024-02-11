2527bdfe508a3fde3f793de4b48bc0babf71357f
Joseph Platta <jwplatta@gmail.com>
2024-02-11 13:41:57 -0600

    move vector store logic out of the api layer into a vector store class

--- server/index.js
+++ server/index.js
@@ -1,40 +1,18 @@
-import * as sqlite_vss from "sqlite-vss";
-import sqlite3 from 'better-sqlite3';
 import express from 'express';
-import path from 'path';
 import cors from 'cors';
-import { pipeline } from '@xenova/transformers';
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import * as tf from '@tensorflow/tfjs';
 import { VectorStore } from './src/db/vector_store.js';
-import {
-  insertEmbeddingsIntoVSS,
-  deleteFromVss,
-  deleteFromNoteChunks,
-  insertNoteChunk,
-  embeddingsQuery
-} from './src/db/sql.js';
-
-//TODO: move all the sql to a separate file
+import { buildDbPath } from './src/util.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+const PORT = process.env.PORT || 3003;
 
-app.get('/check_status', (req, res) => {
-  res.sendStatus(200);
-});
+app.get('/check_status', (req, res) => { res.sendStatus(200) });
 
 app.post('/configure_db', (req, res) => {
-  // TODO: check if running docker container
-  const dbPath = path.join(
-    req.body.vaultPath,
-    req.body.dataStorePath,
-    req.body.dataStoreFilename
-  );
-
   try {
-    new VectorStore(dbPath).configure();
+    new VectorStore(buildDbPath(req.body)).configure();
     res.sendStatus(200);
   } catch (error) {
     console.error(error);
@@ -43,142 +21,59 @@ app.post('/configure_db', (req, res) => {
 });
 
 app.post('/create_embedding', async (req, res) => {
-  const fileName = req.body.fileName;
-  const dbPath = path.join(
-    req.body.vaultPath,
-    req.body.dataStorePath,
-    req.body.dataStoreFilename
-  );
-
-  const vect_db = new VectorStore(dbPath);
-  const filePath = req.body.filePath;
-  const fileContent = req.body.fileContent;
-  const chunkSize = req.body.chunkSize;
-  const model = req.body.model;
-  const embedder = await pipeline('feature-extraction', model);
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: chunkSize,
-    chunkOverlap: 50
-  });
-  const chunks = await splitter.createDocuments([fileContent]);
-  const insertPromises = chunks.map(async(chunk, index) => {
-    try {
-      const embeddings = await embedder([chunk.pageContent]);
-      const meanTensor = tf.tensor(embeddings[0]['data'])
-          .reshape(embeddings[0]['dims'])
-          .mean(0);
-      const embeddingJSON = JSON.stringify(meanTensor.arraySync());
-
-      vect_db.db.prepare(insertNoteChunk)
-        .run(fileName, filePath, chunk.pageContent, embeddingJSON);
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
-    }
-  });
-
-  await Promise.all(insertPromises);
-
-  res.sendStatus(200);
-});
-
-app.post('/delete_embedding', async (req, res) => {
-  const fileName = req.body.fileName;
-  const dbPath = path.join(
-    req.body.vaultPath,
-    req.body.dataStorePath,
-    req.body.dataStoreFilename
-  );
-  const vect_db = new VectorStore(dbPath);
-
-  vect_db.db.exec('BEGIN TRANSACTION');
+  const vect_db = new VectorStore(buildDbPath(req.body));
   try {
-    vect_db.db.prepare(deleteFromVss).run(fileName);
-    vect_db.db.prepare(deleteFromNoteChunks).run(fileName);
-  } catch (error) {
-    vect_db.db.exec('ROLLBACK');
-    console.error("delete file note chunks", ": ", error);
-    res.sendStatus(500);
-  }
-  vect_db.db.exec('COMMIT');
-  res.sendStatus(200);
-});
-
-app.post('/update_index', async (req, res) => {
-  const fileName = req.body.fileName;
-  const dbPath = path.join(
-    req.body.vaultPath,
-    req.body.dataStorePath,
-    req.body.dataStoreFilename
-  );
-  const vect_db = new VectorStore(dbPath);
-
-  // STEP: update the virtual table
-  try {
-    const insertEmbeddingsIntoVSSStmt = vect_db.db.prepare(insertEmbeddingsIntoVSS);
-    insertEmbeddingsIntoVSSStmt.run(fileName);
-  } catch (error) {
-    console.error(insertEmbeddingsIntoVSS, error);
-    res.sendStatus(500);
-  }
-
-  try {
-    const vssCountStmt = vect_db.db.prepare('SELECT count(1) FROM vss_note_chunks');
-    const vssCount = vssCountStmt.pluck().get();
-    console.log("vssCount ", vssCount);
+    await vect_db.embed(
+      req.body.chunkSize,
+      50,
+      req.body.model,
+      req.body.fileContent,
+      req.body.fileName,
+      req.body.filePath
+    )
+    res.sendStatus(200);
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
   }
-
-  res.sendStatus(200);
-
 });
 
-// NOTE: this needs to async
-app.post('/query', async (req, res) => {
-  console.log('QUERY recieved: ', req.body);
+app.post('/delete_embedding', async (req, res) => {
+  try {
+    new VectorStore(buildDbPath(req.body))
+      .deleteFileChunks(req.body.fileName);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 
+app.post('/update_index', async (req, res) => {
+  try {
+    const vect_db = new VectorStore(buildDbPath(req.body));
+    vect_db.updateIndex(req.body.fileName);
+    // console.log("vect_db.size(): ", vect_db.size());
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+app.post('/query', async (req, res) => {
   if (!req.body.query || req.body.query.trim() === '') {
     res.status(200).json([]);
     return;
   }
 
-  const joinedPath = path.join(
-    req.body.vaultPath,
-    req.body.dataStorePath,
-    req.body.dataStoreFilename
-  );
-
-  console.log('database path: ', joinedPath);
-
-  const db = new sqlite3(joinedPath);
-  sqlite_vss.load(db);
-
-  // STEP: embed the query
-  const embedder = await pipeline('feature-extraction', req.body.model);
-  const embeddings = await embedder([req.body.query]);
-  const queryEmbedding = tf.tensor(embeddings[0]['data'])
-    .reshape(embeddings[0]['dims'])
-    .mean(0);
-
-  const searchResultsCount = req.body.searchResultsCount;
-
   try {
-    const vssCountStmt = db.prepare('SELECT count(1) FROM vss_note_chunks');
-    const vssCount = vssCountStmt.pluck().get();
-    console.log("vssCount ", vssCount);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-
-  try {
-    const searchResults = db.prepare(embeddingsQuery).all(
-      "[" + queryEmbedding.arraySync().toString() + "]",
-      searchResultsCount
+    const searchResults = await new VectorStore(buildDbPath(req.body)).query(
+      req.body.query,
+      req.body.searchResultsCount,
+      req.body.model
     );
-    console.log(searchResults);
     res.status(200).json(searchResults);
   } catch (error) {
     console.error(error);
@@ -186,7 +81,6 @@ app.post('/query', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Semantic Search server running on port ${PORT}`);
 });
--- server/src/db/vector_store.js
+++ server/src/db/vector_store.js
@@ -1,7 +1,14 @@
 import sqlite3 from 'better-sqlite3';
-import { checkFileExists } from '../util.js';
 import * as sqlite_vss from "sqlite-vss";
-import { createEmbeddingsTable, createVirtualTable} from './sql.js';
+import { pipeline } from '@xenova/transformers';
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import * as tf from '@tensorflow/tfjs';
+import {
+  countVss, countChunks, createEmbeddingsTable,
+  createVirtualTable, insertNoteChunk,
+  insertEmbeddingsIntoVSS, deleteFromVss,
+  deleteFromNoteChunks, embeddingsQuery
+} from './sql.js';
 
 export class VectorStore {
   constructor(dbPath) {
@@ -9,22 +16,78 @@ export class VectorStore {
     sqlite_vss.load(this.db);
 	}
 
+  chunk_cnt() {
+    try {
+      const chunkCount = this.db.prepare(countChunks).pluck().get()
+      return chunkCount;
+    } catch (error) {
+      throw new Error("Error VectorStore.chunk_cnt: ", error);
+    }
+  }
+
   size() {
-
+    try {
+      const vssCount = this.db.prepare(countVss).pluck().get()
+      return vssCount;
+    } catch (error) {
+      throw new Error("Error VectorStore.size: ", error);
+    }
   }
 
-  embed() {
+  async embed(chunkSize, chunkOverlap, model, fileContent, fileName, filePath) {
+    const embedder = await pipeline('feature-extraction', model);
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: chunkSize,
+      chunkOverlap: chunkOverlap
+    });
+    const chunks = await splitter.createDocuments([fileContent]);
+    return chunks.map(async(chunk, index) => {
+      try {
+        const embeddings = await embedder([chunk.pageContent]);
+        const meanTensor = tf.tensor(embeddings[0]['data'])
+            .reshape(embeddings[0]['dims'])
+            .mean(0);
+        const embeddingJSON = JSON.stringify(meanTensor.arraySync());
 
+        this.db.prepare(insertNoteChunk)
+          .run(fileName, filePath, chunk.pageContent, embeddingJSON);
+      } catch (error) {
+        throw new Error("Error VectorStore.embed: ", error);
+      }
+    });
   }
 
-  updateIndex() {
-
+  updateIndex(fileName) {
+    try{
+      this.db.prepare(insertEmbeddingsIntoVSS).run(fileName);
+    } catch (error) {
+      throw new Error("Error VectorStore.updateIndex: ", error);
+    }
   }
 
   deleteFileChunks(fileName) {
     this.wrapInTransaction(() => {
-      // this.db.run(, [fileName]);
+      this.db.prepare(deleteFromVss).run(fileName);
+      this.db.prepare(deleteFromNoteChunks).run(fileName);
     });
+  }
+
+  async query(queryString, searchResultsCount, model) {
+    const embedder = await pipeline('feature-extraction', model);
+    const embeddings = await embedder([queryString]);
+    const queryEmbedding = tf.tensor(embeddings[0]['data'])
+      .reshape(embeddings[0]['dims'])
+      .mean(0);
+
+    try {
+      const searchResults = this.db.prepare(embeddingsQuery).all(
+        "[" + queryEmbedding.arraySync().toString() + "]",
+        searchResultsCount
+      );
+      return searchResults;
+    } catch (error) {
+      throw new Error("Error VectorStore.query: ", error);
+    }
   }
 
   wrapInTransaction(func) {
@@ -33,43 +96,20 @@ export class VectorStore {
     try {
       func();
       this.db.exec('COMMIT');
+      return true;
     } catch (error) {
       console.error(error);
       this.db.exec('ROLLBACK');
       return false;
     }
-
-    return true;
-  }
-
-  run(stmt, params) {
-    try {
-      return this.db.prepare(stmt).run();
-    } catch (error) {
-      console.error(stmt, error);
-      return false;
-    }
   }
 
   configure() {
-    /**
-     * Configures the vector store database by creating necessary tables and virtual tables.
-     * @returns {boolean} Returns true if the configuration is successful, false otherwise.
-     */
     try {
       this.db.prepare(createEmbeddingsTable).run();
-    } catch (error) {
-      console.error(error);
-      return false;
-    }
-
-    try {
       this.db.prepare(createVirtualTable).run();
     } catch (error) {
-      console.error(error);
-      return false;
+      throw new Error("Error VectorStore.configure: ", error);
     }
-
-    return true;
   }
 }
