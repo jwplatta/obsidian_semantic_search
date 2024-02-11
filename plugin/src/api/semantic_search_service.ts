ec1b997c0b2cecbc3483d6d1517207203d03cf5f
Joseph Platta <jwplatta@gmail.com>
2024-02-11 00:50:19 -0600

    add docstrings explaining api functions

--- plugin/src/api/semantic_search_service.ts
+++ plugin/src/api/semantic_search_service.ts
@@ -1,7 +1,7 @@
 import { Chunk } from 'src/chunk_interface';
 
 // TODO: export class SemanticSearchService
-async function postServer(url: URL, body: Object): Promise<Response>{
+async function post(url: URL, body: Object): Promise<Response>{
   return await fetch(url, {
       method: 'POST',
       headers: {
@@ -11,7 +11,7 @@ async function postServer(url: URL, body: Object): Promise<Response>{
     })
 }
 
-async function getServer(url: URL) {
+async function get(url: URL) {
   return await fetch(url, {
     method: 'GET',
     headers: {
@@ -20,39 +20,64 @@ async function getServer(url: URL) {
   });
 }
 
+/**
+ * Embeds a file by sending its details to the server for embedding,
+ * creating an embedding, and updating the index.
+ *
+ * @param fileDetails - The details of the file to be embedded.
+ * @returns A Promise that resolves when the embedding process is complete.
+ */
 export async function embedFile(fileDetails: Object) {
   try {
-    await postServer(new URL('http://localhost:3003/delete_embedding'), fileDetails);
-    await postServer(new URL('http://localhost:3003/create_embedding'), fileDetails);
-    await postServer(new URL('http://localhost:3003/update_index'), fileDetails);
+    await post(new URL('http://localhost:3003/delete_embedding'), fileDetails);
+    await post(new URL('http://localhost:3003/create_embedding'), fileDetails);
+    await post(new URL('http://localhost:3003/update_index'), fileDetails);
   } catch (error) {
     console.error('Error embedding file:', error);
   }
 }
 
+/**
+ * Checks if the server is available by sending a GET request to the specified URL.
+ * @returns A Promise that resolves to a boolean indicating whether the server is available.
+ */
 export async function serverAvailable(): Promise<boolean> {
-  return await getServer(new URL('http://localhost:3003/check_status'))
-    .then(response => {
-      console.log('Server status: ', response);
+  return await get(new URL('http://localhost:3003/check_status'))
+    .then((response) => {
       return true;
     }).catch(error => {
-      console.error('Server error: ', error);
+      console.error('Error checking server status: ', error);
       return false;
     });
 }
 
+/**
+ * Configures the vector store by sending a POST request to the
+ * specified URL with the provided database details.
+ *
+ * @param dbDetails - The details of the database to be configured.
+ * @returns A promise that resolves to true if the configuration is
+ * successful, or false if there is an error.
+ */
 export async function configureVectorStore(dbDetails: Object) {
-  await postServer(new URL('http://localhost:3003/configure_db'), dbDetails).then(response => {
-    console.log('Vector store configured:', response);
-    return true;
-  }).catch(error => {
-    console.error('Error configuring vector store:', error);
-    return false;
-  });
+  await post(new URL('http://localhost:3003/configure_db'), dbDetails)
+    .then(response => {
+      return true;
+    }).catch(error => {
+      console.error('Error configuring vector store: ', error);
+      return false;
+    });
 }
 
+/**
+ * Queries the note chunks based on the provided query details.
+ *
+ * @param queryDetails - The details of the query.
+ * @returns A promise that resolves to an array of Chunk objects if
+ * the query is successful, otherwise an empty array.
+ */
 export async function queryNoteChunks(queryDetails: Object) : Promise<Chunk[] | []> {
-  const response = await postServer(new URL('http://localhost:3003/query'), queryDetails);
+  const response = await post(new URL('http://localhost:3003/query'), queryDetails);
   if (response.ok) {
     const chunks = await response.json();
     return chunks;
