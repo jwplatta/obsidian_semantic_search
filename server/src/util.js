2090c408d4e17894442b4637b82e9b4f832220e9
Joseph Platta <jwplatta@gmail.com>
2024-02-11 13:41:01 -0600

    util function to build path to sqlite db based on env

--- server/src/util.js
+++ server/src/util.js
@@ -1,4 +1,5 @@
 import fs from 'fs/promises';
+import path from 'path';
 
 export async function checkFileExists(filePath) {
   try {
@@ -6,5 +7,21 @@ export async function checkFileExists(filePath) {
       return true;
   } catch (error) {
       return false;
+  }
+}
+
+export function buildDbPath(reqBody) {
+  try {
+    if (process.env.NODE_ENV === 'docker') {
+      return path.join("/usr/src/server", reqBody.dataStoreFilename);
+    } else {
+      return path.join(
+        reqBody.vaultPath,
+        reqBody.dataStorePath,
+        reqBody.dataStoreFilename
+      );
+    }
+  } catch (error) {
+    throw new Error("Error building db path: ", error);
   }
 }
