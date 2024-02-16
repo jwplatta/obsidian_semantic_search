1bbcd2e8742d95ef49cd799f1ede43cf6d89ea7a
Joseph Platta <jwplatta@gmail.com>
2024-02-15 22:15:32 -0600

    hardcode db name

--- server/src/util.js
+++ server/src/util.js
@@ -11,17 +11,8 @@ export async function checkFileExists (filePath) {
 }
 
 export function buildDbPath (reqBody) {
-  try {
-    if (process.env.NODE_ENV === 'docker') {
-      return path.join('/usr/src/server', reqBody.dataStoreFilename)
-    } else {
-      return path.join(
-        reqBody.vaultPath,
-        reqBody.dataStorePath,
-        reqBody.dataStoreFilename
-      )
-    }
-  } catch (error) {
-    throw new Error('Error building db path: ', error)
-  }
+  return path.join(
+    reqBody.pluginPath,
+    'semantic_search.db'
+  )
 }
