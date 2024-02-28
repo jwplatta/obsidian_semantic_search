abc21fb7af426f62a50088977aaf4dfc4824b85e
Joseph Platta <jwplatta@gmail.com>
2024-02-27 23:54:43 -0600

    - use interfaces - function to tell server to get currently embedded files - function to tell server to delete all embeddings

--- plugin/src/api/semantic_search_service.ts
+++ plugin/src/api/semantic_search_service.ts
@@ -1,8 +1,4 @@
-import { Chunk } from 'src/chunk_interface';
-import { DbDetails } from 'src/db_details_interface';
-import { FileDetails } from 'src/file_details_interface';
-import { QueryDetails } from 'src/query_details_interface';
-
+import { Chunk, DbDetails, FileDetails, QueryDetails, EmbeddingParams } from 'src/interfaces';
 
 async function get(url: URL) {
     return await fetch(url, {
@@ -29,6 +25,48 @@ export async function embeddingsInfo(dbDetails: DbDetails) {
     }
 }
 
+interface EmbeddedFile {
+    file_name: string;
+}
+
+export async function embeddedFiles(dbDetails: DbDetails) {
+    try {
+        const response = await fetch(new URL('http://localhost:3003/embedded_files'), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(dbDetails)
+        });
+        const responseJSON = await response.json();
+        const fileNames = responseJSON.map((file: EmbeddedFile) => file.file_name);
+        return fileNames;
+    } catch (error) {
+        console.error('Error getting embedded files: ', error, dbDetails);
+    }
+}
+
+export async function resetEmbeddingIndex({ vaultPath: vaultPath, pluginPath: pluginPath }: DbDetails) {
+    try {
+        const response = await fetch(new URL('http://localhost:3003/reset'), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(
+                {
+                    vaultPath: vaultPath,
+                    pluginPath: pluginPath
+                }
+            )
+        });
+        console.log(response);
+        return true;
+    } catch(error) {
+        console.error('Error resetting embedding index:', error);
+    }
+}
+
 /**
  * Embeds a file by any deleting existing embedding,
  * creating a new embedding, and updating the virtual index.
@@ -36,32 +74,29 @@ export async function embeddingsInfo(dbDetails: DbDetails) {
  * @param fileDetails - The details of the file to be embedded.
  * @returns A Promise that resolves when the embedding process is complete.
  */
-export async function embedFile(file: FileDetails) {
+export async function embedFile(file: FileDetails, embeddingParams: EmbeddingParams) {
     try {
         return await fetch(new URL('http://localhost:3003/embed_file'), {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(file)
+            body: JSON.stringify({ ...file, ...embeddingParams })
         });
     } catch (error) {
         console.error('Error embedding file:', error, file);
     }
 }
 
-export async function embedBatch(files: FileDetails[], embeddingModel: string, vaultPath: string, pluginPath: string, chunkSize: number) {
+export async function embedBatch(files: FileDetails[], embeddingParams: EmbeddingParams) {
     return await fetch(new URL('http://localhost:3003/embed_batch'), {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            model: embeddingModel,
-            vaultPath: vaultPath,
-            pluginPath: pluginPath,
-            chunkSize: chunkSize,
-            files: files
+            files: files,
+            ...embeddingParams
         })
     });
 }
@@ -76,7 +111,7 @@ export async function updateEmbeddingIndex(dbDetails: DbDetails) {
             body: JSON.stringify(dbDetails)
         });
     } catch (error) {
-        console.error('Error updating embedding index:', error, dbDetails);
+        console.error('Error updating embedding index:', error);
     }
 }
 
@@ -137,8 +172,6 @@ export async function queryNoteChunks(queryDetails: QueryDetails): Promise<Chunk
         },
         body: JSON.stringify(queryDetails)
     });
-
-    console.log(response);
 
     if (response.ok) {
         const chunks = await response.json();
